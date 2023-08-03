9580d582b449552de7ab079f0035ad0f454f0a6e
Arun Kumar <aruncode99@gmail.com>
2023-08-03 18:05:43 +0530

    first commit-setting up the project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h1 className="m-10 bg-red-500">Youtube Clone</h1>
+    </div>
+  );
+}
+
+export default App;
