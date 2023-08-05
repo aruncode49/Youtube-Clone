10a59597c1c4372e7e6573bdc9ef4a6ae87269c9
Arun Kumar <aruncode99@gmail.com>
2023-08-05 14:44:59 +0530

    complete button list of main-container

--- src/components/ButtonList.jsx
+++ src/components/ButtonList.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import BodyButton from "./BodyButton";
 
 const ButtonList = () => {
-  return <div>ButtonList</div>;
+  return (
+    <div className="flex py-2 overflow-x-auto">
+      <BodyButton name="All" />
+      <BodyButton name="Gaming" />
+      <BodyButton name="Songs" />
+      <BodyButton name="Live" />
+      <BodyButton name="Soccer" />
+      <BodyButton name="Cricket" />
+      <BodyButton name="Cooking" />
+      <BodyButton name="News" />
+      <BodyButton name="History" />
+      <BodyButton name="Computer Science" />
+    </div>
+  );
 };
 
 export default ButtonList;
---
+++ src/utils/constants.js
@@ -0,0 +1,5 @@
+const GOOGLE_API_KEY = "AIzaSyD1nPrgKFfBTsQrAlFs7bGCDIx8H70JZ48";
+
+export const YOUTUBE_VIDEOS_API =
+  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&key=" +
+  GOOGLE_API_KEY;
