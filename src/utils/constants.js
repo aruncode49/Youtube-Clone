753e80a002533422430a43c67b93611de97aa5e3
Arun Kumar <aruncode99@gmail.com>
2023-08-07 20:10:37 +0530

    solved the bug of http over https

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,7 +5,7 @@ export const YOUTUBE_VIDEOS_API =
   GOOGLE_API_KEY;
 
 export const YOUTUBE_SEARCH_API =
-  "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
+  "https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
 
 // Calculate View function
 export function calculateViews(views) {
