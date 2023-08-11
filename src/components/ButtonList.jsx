78d66356ea8ffa063113d9948b185409cfaeff53
Arun Kumar <aruncode99@gmail.com>
2023-08-11 16:21:35 +0530

    completed working of button list

--- src/components/ButtonList.jsx
+++ src/components/ButtonList.jsx
@@ -1,21 +1,42 @@
 import React from "react";
 import BodyButton from "./BodyButton";
+import { Link } from "react-router-dom";
 
 const ButtonList = () => {
   return (
     <div className="fixed mt-[3.75rem] md:ml-[78px] w-full  bg-white top flex py-2 overflow-x-auto">
       <div className="flex">
         <BodyButton name="All" />
-        <BodyButton name="Gaming" />
-        <BodyButton name="Songs" />
-        <BodyButton name="Live" />
-        <BodyButton name="Soccer" />
-        <BodyButton name="Cricket" />
-        <BodyButton name="Cooking" />
-        <BodyButton name="News" />
-        <BodyButton name="History" />
-        <BodyButton name="Coding" />
-        <BodyButton name="GeeksForGeeks" />
+        <Link to={`/search/gaming`}>
+          <BodyButton name="Gaming" />
+        </Link>
+        <Link to={`/search/songs`}>
+          <BodyButton name="Songs" />
+        </Link>
+        <Link to={`/search/live`}>
+          <BodyButton name="Live" />
+        </Link>
+        <Link to={`/search/soccer`}>
+          <BodyButton name="Soccer" />
+        </Link>
+        <Link to={`/search/cricket`}>
+          <BodyButton name="Cricket" />
+        </Link>
+        <Link to={`/search/cooking`}>
+          <BodyButton name="Cooking" />
+        </Link>
+        <Link to={`/search/news`}>
+          <BodyButton name="News" />
+        </Link>
+        <Link to={`/search/history`}>
+          <BodyButton name="History" />
+        </Link>
+        <Link to={`/search/coding`}>
+          <BodyButton name="Coding" />
+        </Link>
+        <Link to={`/search/geeksforgeeks`}>
+          <BodyButton name="GeeksForGeeks" />
+        </Link>
       </div>
     </div>
   );
